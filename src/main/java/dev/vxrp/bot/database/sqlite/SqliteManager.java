e55d96878ec29667c4f8b43b8061f459ac13738d
Vxrpenter <110356385+Vxrpenter@users.noreply.github.com>
2024-10-23 17:28:00 +0200

    Forgot to add channel and message ids to the table

--- src/main/java/dev/vxrp/bot/database/sqlite/SqliteManager.java
+++ src/main/java/dev/vxrp/bot/database/sqlite/SqliteManager.java
@@ -18,29 +18,39 @@ public class SqliteManager {
             statement.execute(
                     "CREATE TABLE IF NOT EXISTS notice_of_departure (" +
                     "id TEXT PRIMARY KEY," +
+                    "channel_message_id TEXT NOT NULL," +
                     "start_time TEXT NOT NULL," +
                     "end_time TEXT NOT NULL" +
                     ");");
+            logger.info("{} - Set up table {} with rows: {}, {}, {}, {}",
+                    ColorTool.apply(DCColor.GOLD, ColorTool.apply(DCColor.BOLD, "SQLite")),
+                    ColorTool.apply(DCColor.GOLD, "notice_of_departure"),
+                    ColorTool.apply(DCColor.GREEN, "id"),
+                    ColorTool.apply(DCColor.GREEN, "channel_message_id"),
+                    ColorTool.apply(DCColor.GREEN, "start_time"),
+                    ColorTool.apply(DCColor.GREEN, "end_time"));
         }
         logger.info("{} - Set up Sqlite database correctly",
                 ColorTool.apply(DCColor.GOLD, ColorTool.apply(DCColor.BOLD, "SQLite")));
     }
 
-    public void addNoticeOfDeparture(String id,String start_time, String end_time) throws SQLException {
-        try (PreparedStatement statement = connection.prepareStatement("INSERT INTO notice_of_departure VALUES (?,?,?)")) {
+    public void addNoticeOfDeparture(String id, String channel_message_id, String start_time, String end_time) throws SQLException {
+        try (PreparedStatement statement = connection.prepareStatement("INSERT INTO notice_of_departure VALUES (?,?,?,?)")) {
             statement.setString(1, id);
-            statement.setString(2, start_time);
-            statement.setString(3, end_time);
+            statement.setString(2, channel_message_id);
+            statement.setString(3, start_time);
+            statement.setString(4, end_time);
             statement.executeUpdate();
-            logger.info("{} - Added notice of departure - id:{} , start_time:{} , end_time:{}",
+            logger.info("{} - Added notice of departure - id: {}, channel_message_id: {} , start_time: {} , end_time: {}",
                     ColorTool.apply(DCColor.GOLD, ColorTool.apply(DCColor.BOLD, "SQLite")),
                     ColorTool.apply(DCColor.GREEN, id),
+                    ColorTool.apply(DCColor.GREEN, channel_message_id),
                     ColorTool.apply(DCColor.GOLD, start_time),
                     ColorTool.apply(DCColor.GOLD, end_time));
         }
     }
 
-    public void updateNoticeOfDeparture(String id,String start_time, String end_time) throws SQLException {
+    public void updateNoticeOfDeparture(String id, String channel_message_id, String start_time, String end_time) throws SQLException {
         try (PreparedStatement statement = connection.prepareStatement("UPDATE notice_of_departure SET start_time=?, end_time=? WHERE id=?")) {
             statement.setString(1, start_time);
             statement.setString(2, end_time);
@@ -51,6 +61,18 @@ public class SqliteManager {
                     ColorTool.apply(DCColor.GREEN, id),
                     ColorTool.apply(DCColor.GOLD, start_time),
                     ColorTool.apply(DCColor.GOLD, end_time));
+        }
+    }
+
+    public void updateChannelMessageId(String id, String channel_message_id) throws SQLException {
+        try (PreparedStatement statement = connection.prepareStatement("UPDATE notice_of_departure SET start_time=? WHERE id=?")) {
+            statement.setString(1, channel_message_id);
+            statement.setString(2, id);
+            statement.executeUpdate();
+            logger.info("{} - Updated start_time - id: {} , channel_message_id: {}",
+                    ColorTool.apply(DCColor.GOLD, ColorTool.apply(DCColor.BOLD, "SQLite")),
+                    ColorTool.apply(DCColor.GREEN, id),
+                    ColorTool.apply(DCColor.GOLD, channel_message_id));
         }
     }
 
@@ -92,7 +114,7 @@ public class SqliteManager {
         try (PreparedStatement statement = connection.prepareStatement("SELECT * FROM notice_of_departure WHERE id=?")) {
             statement.setString(1, id);
             try (ResultSet resultSet = statement.executeQuery()) {
-                return new NoticeOfDeparture(resultSet.getString("start_time"), resultSet.getString("end_time"));
+                return new NoticeOfDeparture(resultSet.getString("channel_message_id"), resultSet.getString("start_time"), resultSet.getString("end_time"));
             }
         }
     }
@@ -111,6 +133,15 @@ public class SqliteManager {
             statement.setString(1, id);
             try (ResultSet resultSet = statement.executeQuery()) {
                 return resultSet.getString("end_time");
+            }
+        }
+    }
+
+    public String getChannelMessageId(String id) throws SQLException {
+        try (PreparedStatement statement = connection.prepareStatement("SELECT FROM channel_message_id WHERE id=?")) {
+            statement.setString(1, id);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                return resultSet.getString("channel_message_id");
             }
         }
     }
--- src/main/java/dev/vxrp/bot/util/objects/NoticeOfDeparture.java
+++ src/main/java/dev/vxrp/bot/util/objects/NoticeOfDeparture.java
@@ -1,4 +1,4 @@
 package dev.vxrp.bot.util.objects;
 
-public record NoticeOfDeparture(String start_time, String end_time) {
+public record NoticeOfDeparture(String channel_message_id, String start_time, String end_time) {
 }
