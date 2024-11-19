b7f8e0a74c3e9c889916594a0ff6b1f65eafc2fc
vxrpenter <david.reulen@web.de>
2024-11-19 16:01:48 +0100

    New command management system

--- src/main/kotlin/dev/vxrp/bot/BotManager.kt
+++ src/main/kotlin/dev/vxrp/bot/BotManager.kt
@@ -4,7 +4,6 @@ import dev.vxrp.bot.commands.CommandManager
 import dev.vxrp.configuration.loaders.Config
 import net.dv8tion.jda.api.JDA
 import net.dv8tion.jda.api.JDABuilder
-import net.dv8tion.jda.api.interactions.commands.build.Commands
 
 class BotManager(val config: Config) {
     init {
@@ -14,11 +13,7 @@ class BotManager(val config: Config) {
     }
 
     private fun initializeCommands(api: JDA) {
-        CommandManager(api, config)
-            .registerCommand(Commands.slash("test1", "test1"))
-            .registerCommand(Commands.slash("test2", "test2"))
-            .registerCommand(Commands.slash("test3", "test3"))
-            .registerCommand(Commands.slash("test4", "test4"))
-            .initialize()
+
+        CommandManager(api, config, "/configs/commands.json").registerCommands()
     }
 }
--- src/main/kotlin/dev/vxrp/bot/commands/CommandManager.kt
+++ src/main/kotlin/dev/vxrp/bot/commands/CommandManager.kt
@@ -1,25 +1,86 @@
 package dev.vxrp.bot.commands
 
 import dev.vxrp.configuration.loaders.Config
+import dev.vxrp.configuration.managers.ConfigManager
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
 import net.dv8tion.jda.api.JDA
-import net.dv8tion.jda.api.interactions.commands.build.CommandData
+import net.dv8tion.jda.api.Permission
+import net.dv8tion.jda.api.interactions.commands.Command
+import net.dv8tion.jda.api.interactions.commands.DefaultMemberPermissions
+import net.dv8tion.jda.api.interactions.commands.OptionType
+import net.dv8tion.jda.api.interactions.commands.build.Commands
+import net.dv8tion.jda.api.interactions.commands.build.OptionData
 import org.slf4j.LoggerFactory
+import java.io.File
 
-class CommandManager(val api: JDA, val config: Config) {
+@Serializable
+data class CommandList(val commands: List<CustomCommand>)
+
+@Serializable
+data class CustomCommand(val active: Boolean,
+                         val name: String,
+                         val description: String,
+                         @SerialName("default_permissions")
+                         val defaultPermissions: List<String>,
+                         val options: List<Options>)
+
+@Serializable
+data class Options(val type: String, val name: String, val description: String, val isRequired: Boolean, val choices: List<Choices>)
+
+@Serializable
+data class Choices(val name: String, val id: String)
+
+class CommandManager(val api: JDA, val config: Config, val file: String) {
     private val logger = LoggerFactory.getLogger(CommandManager::class.java)
-    private val commands = mutableListOf<CommandData>()
+    private val dir = System.getProperty("user.dir")
 
-    fun registerCommand(command: CommandData) : CommandManager {
-        commands.add(command)
-        return this
+    init {
+        val content = ConfigManager::class.java.getResourceAsStream(file)
+
+        val currentFile = File("$dir$file")
+        if (!currentFile.exists()) {
+            currentFile.createNewFile()
+            logger.info("Created commands configuration file $dir$file")
+
+            if (content != null) {
+                currentFile.appendBytes(content.readBytes())
+                logger.info("Wrote contents to $dir$file")
+            }
+        }
     }
 
-    fun initialize() {
-        for (command in commands) {
-            if (!config.commands.contains(command.name)) continue
-            api.updateCommands().addCommands(command)
+    fun registerCommands() {
+        val data = Json.decodeFromString<CommandList>(query())
+
+        for (command in data.commands) {
+            if (!command.active) continue
+
+            val permissions = mutableListOf<Permission>()
+            command.defaultPermissions.forEach { permission -> permissions.add(Permission.valueOf(permission)) }
+
+            val currentCommand = Commands.slash(command.name, command.description).also { commandData ->
+                if (command.options.isNotEmpty()) {
+                    val optionData = mutableListOf<OptionData>()
+                    for (option in command.options) {
+                        val choices = mutableListOf<Command.Choice>()
+
+                        if (option.choices.isNotEmpty()) { repeat(option.choices.size) { choices.add(Command.Choice(option.name, option.description))} }
+                        optionData.add(OptionData(OptionType.valueOf(option.type), option.name, option.description, option.isRequired).also { if (choices.isNotEmpty()) { it.addChoices(choices) } })
+                    }
+                    commandData.addOptions(optionData)
+                }
+            }.setDefaultPermissions(DefaultMemberPermissions.enabledFor(permissions))
+
+            api.updateCommands().addCommands(currentCommand)
             logger.info("Registering command ${command.name}")
         }
+    }
+
+    private fun query() : String {
+        val currentFile = File("$dir$file")
+        return currentFile.readText()
     }
 }
 
