ffc3b2a6a4066e1395a0588f03e5f234803b9b59
vxrpenter <david.reulen@web.de>
2024-11-20 19:13:43 +0100

    Kotlinx Coroutines and jda-ktx implementation

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,20 +21,26 @@ java {
     targetCompatibility = JavaVersion.VERSION_22
 }
 
+val kotlinxCoroutinesVersion = "1.9.0"
+
 val exposedVersion = "0.56.0"
 val sqliteVersion = "3.45.1.0"
 val mySqlVersion = "9.1.0"
 val postgresSqlVersion = "42.7.2"
 val mariaDBVersion = "3.5.0"
 
-val kotlinVersion = "1.7.3"
+val kotlinxSerializationVersion = "1.7.3"
 val kamlVersion = "0.65.0"
 val jdaVersion = "5.2.1"
+val jdaKtxVersion = "0.12.0"
 val logbackVersion = "1.5.6"
 val apacheCommonsVersion = "3.4"
 val gsonVersion = "2.11.0"
 
 dependencies {
+    // Default
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$kotlinxCoroutinesVersion")
+
     // Database tools and drivers
     implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
     implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
@@ -44,12 +50,13 @@ dependencies {
     implementation("org.mariadb.jdbc:mariadb-java-client:$mariaDBVersion")
 
     // Config and Serialization
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:$kotlinVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:$kotlinxSerializationVersion")
     implementation("com.charleskorn.kaml:kaml:$kamlVersion")
 
     // Discord api implementation + Logback
     implementation("net.dv8tion:JDA:$jdaVersion")
     implementation("ch.qos.logback:logback-classic:$logbackVersion")
+    implementation("club.minnced:jda-ktx:$jdaKtxVersion")
 
     // String and JSON tooling
     implementation("org.apache.commons:commons-lang3:$apacheCommonsVersion")
