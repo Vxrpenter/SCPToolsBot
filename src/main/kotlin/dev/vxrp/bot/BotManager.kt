54eb453ae2577d00ef5e0182288ca73422d6fb50
My Username <Vxrpenter@users.noreply.github.com>
2024-11-20 17:30:23 +0100

    Activity implementation with safety for type not matching

--- src/main/kotlin/dev/vxrp/bot/BotManager.kt
+++ src/main/kotlin/dev/vxrp/bot/BotManager.kt
@@ -4,15 +4,27 @@ import dev.vxrp.bot.commands.CommandManager
 import dev.vxrp.configuration.loaders.Config
 import dev.vxrp.database.sqlite.SqliteManager
 import net.dv8tion.jda.api.JDABuilder
+import net.dv8tion.jda.api.entities.Activity
+import net.dv8tion.jda.api.entities.Activity.ActivityType
+import net.dv8tion.jda.api.requests.GatewayIntent
+import net.dv8tion.jda.api.utils.cache.CacheFlag
 
 class BotManager(val config: Config) {
     init {
-        val api = JDABuilder.createDefault(config.token).build()
+        val api = JDABuilder.createDefault(config.token, GatewayIntent.MESSAGE_CONTENT, GatewayIntent.GUILD_MESSAGES, GatewayIntent.GUILD_MEMBERS).apply {
+            setActivity(Activity.of(
+                ActivityType.CUSTOM_STATUS.takeIf { !enumContains<ActivityType>(config.activityType) } ?: ActivityType.valueOf(config.activityType),
+                config.activityContent))
+        }
+            .disableCache(CacheFlag.VOICE_STATE, CacheFlag.EMOJI, CacheFlag.STICKER, CacheFlag.SCHEDULED_EVENTS)
+            .build()
 
         CommandManager(api, config, "/configs/commands.json").registerCommands()
         SqliteManager(config,"database", "data.db")
+
     }
 
-
-
+    private inline fun <reified T : Enum<T>> enumContains(name: String): Boolean {
+        return enumValues<T>().any { it.name == name}
+    }
 }
