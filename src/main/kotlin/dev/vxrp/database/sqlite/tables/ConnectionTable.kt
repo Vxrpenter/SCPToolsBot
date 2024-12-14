8e6a2b3c629a6e80f28459ec4008f21a63c5b829
My Username <Vxrpenter@users.noreply.github.com>
2024-12-14 19:00:08 +0100

    Added maintenance field to connection table

--- src/main/kotlin/dev/vxrp/database/sqlite/tables/ConnectionTable.kt
+++ src/main/kotlin/dev/vxrp/database/sqlite/tables/ConnectionTable.kt
@@ -6,6 +6,7 @@ class ConnectionTable {
     object Connections : Table("connections") {
         val id = text("id")
         val status = bool("status")
+        val maintenance = bool("maintenance")
 
         override val primaryKey: PrimaryKey
             get() = PrimaryKey(id)
