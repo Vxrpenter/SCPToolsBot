7ba2c41e310bd98d3408be222e037657cf3e90ca
My Username <Vxrpenter@users.noreply.github.com>
2025-01-31 14:29:46 +0100

    Working editing

--- src/main/kotlin/dev/vxrp/bot/events/stringSelectMenus/ApplicationStringSelectMenus.kt
+++ src/main/kotlin/dev/vxrp/bot/events/stringSelectMenus/ApplicationStringSelectMenus.kt
@@ -1,6 +1,6 @@
 package dev.vxrp.bot.events.stringSelectMenus
 
-import dev.vxrp.bot.modals.ApplicationModals
+import dev.vxrp.bot.modals.ApplicationModalSave
 import dev.vxrp.configuration.loaders.Config
 import dev.vxrp.configuration.loaders.Translation
 import net.dv8tion.jda.api.events.interaction.component.StringSelectInteractionEvent
@@ -8,7 +8,7 @@ import net.dv8tion.jda.api.events.interaction.component.StringSelectInteractionE
 class ApplicationStringSelectMenus(val event: StringSelectInteractionEvent, val config: Config, val translation: Translation) {
     init {
         if (event.selectMenu.id?.startsWith("application_activation_add") == true) {
-            event.replyModal(ApplicationModals(translation).chooseCountModal( event.selectedOptions[0].value)).queue()
+            event.replyModal(ApplicationModalSave(translation).chooseCountModal(event.selectedOptions[0].value, event.selectMenu.id?.split(":")?.get(2)!!)).queue()
         }
 
         if (event.selectMenu.id?.startsWith("application_activation_remove") == true) {
