c171152711b885ce5c93c0226a68cf41d415f6d7
My Username <Vxrpenter@users.noreply.github.com>
2025-02-17 13:43:54 +0100

    Message implementation in manager

--- src/main/kotlin/dev/vxrp/bot/permissions/PermissionMessageHandler.kt
+++ src/main/kotlin/dev/vxrp/bot/permissions/PermissionMessageHandler.kt
@@ -1,4 +1,35 @@
 package dev.vxrp.bot.permissions
 
-class PermissionMessageHandler {
+import dev.vxrp.bot.permissions.enums.PermissionType
+import dev.vxrp.configuration.loaders.Config
+import dev.vxrp.configuration.loaders.Translation
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+class PermissionMessageHandler(val config: Config, val translation: Translation) {
+    private val logger: Logger = LoggerFactory.getLogger(PermissionMessageHandler::class.java)
+
+    fun sendSpecifiedMessage(permissionType: PermissionType, permitted: Boolean) {
+        when (permissionType) {
+            PermissionType.TICKET -> {
+
+            }
+
+            PermissionType.TICKET_LOGS -> {
+
+            }
+
+            PermissionType.REGULARS -> {
+
+            }
+
+            PermissionType.STATUS_BOT -> {
+
+            }
+
+            PermissionType.APPLICATION -> {
+
+            }
+        }
+    }
 }
