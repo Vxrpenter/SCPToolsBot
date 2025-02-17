7d6dd39b09bd7873167f53053446a14037578065
My Username <Vxrpenter@users.noreply.github.com>
2025-02-17 12:26:45 +0100

    Made applications roleId specific

--- src/main/kotlin/dev/vxrp/database/tables/ApplicationTable.kt
+++ src/main/kotlin/dev/vxrp/database/tables/ApplicationTable.kt
@@ -8,7 +8,9 @@ import org.jetbrains.exposed.sql.transactions.transaction
 class ApplicationTable {
     object Applications : Table("applications") {
         val id = text("id")
+        val roleId = text("roleId")
         val state = bool("state")
+        val result = bool("result")
         val issuer = text("issuer")
         val handler = text("handler").nullable()
 
@@ -16,21 +18,23 @@ class ApplicationTable {
             get() = PrimaryKey(id)
     }
 
-    fun addToDatabase(id: String, state: Boolean, issuer: String, handler: String?) {
+    fun addToDatabase(id: String, roleId: String, state: Boolean, result: Boolean, issuer: String, handler: String?) {
         transaction {
             Applications.insert {
                 it[Applications.id] = id
+                it[Applications.roleId] = roleId
                 it[Applications.state] = state
+                it[Applications.result] = result
                 it[Applications.issuer] = issuer
                 it[Applications.handler] = handler
             }
         }
     }
 
-    fun retrieveSerial(): Long {
+    fun retrieveSerial(roleId: String): Long {
         var count: Long = 0
         transaction {
-            count = Applications.selectAll().count()
+            count = Applications.selectAll().where{Applications.roleId eq roleId}.count()
         }
 
         return count
