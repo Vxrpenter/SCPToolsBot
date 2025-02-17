a9a16df43f0d22854367986d62059a21fc3940b1
My Username <Vxrpenter@users.noreply.github.com>
2025-02-17 15:24:09 +0100

    Ticket function with Applications in a database

--- src/main/kotlin/dev/vxrp/database/tables/TicketTable.kt
+++ src/main/kotlin/dev/vxrp/database/tables/TicketTable.kt
@@ -3,10 +3,12 @@ package dev.vxrp.database.tables
 import dev.vxrp.bot.ticket.enums.TicketStatus
 import dev.vxrp.bot.ticket.enums.TicketType
 import net.dv8tion.jda.api.entities.User
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
 import org.jetbrains.exposed.sql.Table
 import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.transactions.transaction
+import org.jetbrains.exposed.sql.update
 import java.time.LocalDate
 
 class TicketTable {
@@ -53,5 +55,47 @@ class TicketTable {
         }
 
         return type!!
+    }
+
+    fun updateTicketStatus(ticketId: String, ticketStatus: TicketStatus) {
+        transaction {
+            TicketTable.Tickets.update({ TicketTable.Tickets.id.eq(ticketId) }) {
+                it[status] = ticketStatus.toString()
+            }
+        }
+    }
+
+    fun getTicketStatus(ticketId: String): TicketStatus? {
+        var status: TicketStatus? = null
+        transaction {
+            TicketTable.Tickets.selectAll()
+                .where(TicketTable.Tickets.id.eq(ticketId))
+                .forEach {
+                    status = TicketStatus.valueOf(it[TicketTable.Tickets.status])
+                }
+        }
+
+        return status
+    }
+
+    fun getTicketType(ticketId: String): TicketType? {
+        var type: TicketType? = null
+        transaction {
+            TicketTable.Tickets.selectAll()
+                .where(TicketTable.Tickets.id.eq(ticketId))
+                .forEach {
+                    type = TicketType.valueOf(it[TicketTable.Tickets.type])
+                }
+        }
+
+        return type
+    }
+
+    fun updateTicketHandler(ticketId: String, userId: String) {
+        transaction {
+            TicketTable.Tickets.update({ TicketTable.Tickets.id.eq(ticketId) }) {
+                it[handler] = userId
+            }
+        }
     }
 }
