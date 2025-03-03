9554587ecc5e281fbc74294ebecdce177dfa2d13
My Username <Vxrpenter@users.noreply.github.com>
2025-03-03 21:39:28 +0100

    Removed almost all deprecated java code

--- src/main/java/dev/vxrp/util/records/noticeOfDeparture/NoticeOfDeparture.java
+++
@@ -1,4 +0,0 @@
-package dev.vxrp.util.records.noticeOfDeparture;
-
-public record NoticeOfDeparture(String id,String channel_id, String message_id, String start_time, String end_time) {
-}
--- src/main/java/dev/vxrp/util/records/regular/RoleHierarchy.java
+++
@@ -1,6 +0,0 @@
-package dev.vxrp.util.records.regular;
-
-import net.dv8tion.jda.api.entities.Role;
-
-public record RoleHierarchy(int hierarchyPlace, Role role) {
-}
