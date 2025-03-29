e59c552ab0a32364d909e82c7b9b270dd8f7ac9f
vxrpenter <david.reulen@web.de>
2025-03-29 23:30:58 +0100

    Modal + Button implementation for notice of departures

--- src/main/kotlin/dev/vxrp/bot/events/buttons/NoticeOfDepartureButtons.kt
+++ src/main/kotlin/dev/vxrp/bot/events/buttons/NoticeOfDepartureButtons.kt
@@ -1,8 +1,14 @@
 package dev.vxrp.bot.events.buttons
 
+import dev.vxrp.bot.modals.NoticeOfDepartureModals
 import dev.vxrp.configuration.loaders.Config
 import dev.vxrp.configuration.loaders.Translation
 import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent
 
 class NoticeOfDepartureButtons(val event: ButtonInteractionEvent, val config: Config, val translation: Translation) {
+    init {
+        if (event.button.id?.startsWith("file_notice_of_departure") == true) {
+            event.replyModal(NoticeOfDepartureModals(translation).generalModal()).queue()
+        }
+    }
 }
