7f1e4b8b3e0bdb1473628b687ff1c18594c41d2e
vxrpenter <david.reulen@web.de>
2025-03-29 23:23:21 +0100

    Added notice of departure button listener

---
+++ src/main/kotlin/dev/vxrp/bot/events/buttons/NoticeOfDepartureButtons.kt
@@ -0,0 +1,8 @@
+package dev.vxrp.bot.events.buttons
+
+import dev.vxrp.configuration.loaders.Config
+import dev.vxrp.configuration.loaders.Translation
+import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent
+
+class NoticeOfDepartureButtons(val event: ButtonInteractionEvent, val config: Config, val translation: Translation) {
+}
