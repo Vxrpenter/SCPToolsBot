5da83e3899cc21db2a1529c415e2f28f4570228f
vxrpenter <david.reulen@web.de>
2025-03-29 16:49:51 +0100

    New ticket config

--- src/main/kotlin/dev/vxrp/bot/ticket/data/Ticket.kt
+++ src/main/kotlin/dev/vxrp/bot/ticket/data/Ticket.kt
@@ -45,17 +45,8 @@ data class TicketTypes(
 data class TicketChildRules(
     @SerialName("parent_name")
     val parentName: String,
-    @SerialName("allow_settings")
-    val allowSettings: Boolean,
     @SerialName("use_status_bar")
     val useStatusBar: Boolean,
-    val logging: TicketLogging,
     @SerialName("lock_on_default")
     val lockOnDefault: Boolean
-)
-
-@Serializable
-data class TicketLogging(
-    val messages: Boolean,
-    val files: Boolean
 )
