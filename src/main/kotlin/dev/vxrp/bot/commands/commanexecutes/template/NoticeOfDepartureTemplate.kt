2a60b0e3eb9b1778ea00ddd71433053d53d1cd2d
vxrpenter <david.reulen@web.de>
2025-03-29 23:12:17 +0100

    Implemented the notice of departure template

--- src/main/kotlin/dev/vxrp/bot/commands/commanexecutes/template/NoticeOfDepartureTemplate.kt
+++ src/main/kotlin/dev/vxrp/bot/commands/commanexecutes/template/NoticeOfDepartureTemplate.kt
@@ -1,4 +1,24 @@
 package dev.vxrp.bot.commands.commanexecutes.template
 
-class NoticeOfDepartureTemplate {
+import dev.minn.jda.ktx.messages.Embed
+import dev.minn.jda.ktx.messages.send
+import dev.vxrp.configuration.loaders.Config
+import dev.vxrp.configuration.loaders.Translation
+import net.dv8tion.jda.api.entities.emoji.Emoji
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
+import net.dv8tion.jda.api.interactions.components.buttons.Button
+
+class NoticeOfDepartureTemplate(val config: Config, val translation: Translation) {
+    fun pasteTemplate(event: SlashCommandInteractionEvent) {
+        val embed = Embed {
+            title = translation.noticeOfDeparture.embedTemplateTitle
+            description = translation.noticeOfDeparture.embedTemplateBody
+        }
+
+        event.channel.send("", listOf(embed)).setActionRow(
+            Button.success("file_notice_of_departure", translation.buttons.textNoticeOfDepartureFile).withEmoji(Emoji.fromFormatted("⏰"))
+        ).queue()
+
+        event.reply("Created notice of departure template").setEphemeral(true).queue()
+    }
 }
