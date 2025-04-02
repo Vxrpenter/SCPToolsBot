664928d8f0bf4b4116f1ab70dfdfb6d872931cdc
vxrpenter <david.reulen@web.de>
2025-04-03 01:38:13 +0200

    New coroutine scope

--- src/main/kotlin/dev/vxrp/util/CoroutineScopes.kt
+++ src/main/kotlin/dev/vxrp/util/CoroutineScopes.kt
@@ -1,6 +1,7 @@
 package dev.vxrp.util
 
 import dev.vxrp.bot.BotManager
+import dev.vxrp.bot.noticeofdeparture.NoticeOfDepartureManager
 import dev.vxrp.bot.status.StatusManager
 import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.CoroutineScope
@@ -8,6 +9,11 @@ import org.slf4j.LoggerFactory
 
 val statusbotScope = CoroutineScope(CoroutineExceptionHandler { _, exception ->
     LoggerFactory.getLogger(StatusManager::class.java).error("An error occurred in the statusbot coroutine", exception)
+})
+
+val noticeOfDepartureScope = CoroutineScope(CoroutineExceptionHandler { _, exception ->
+    LoggerFactory.getLogger(NoticeOfDepartureManager::class.java)
+        .error("An error occurred in the notice of departure coroutine", exception)
 })
 
 val defaultStatusScope = CoroutineScope(CoroutineExceptionHandler { _, exception ->
