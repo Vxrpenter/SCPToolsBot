99ccbf996421f5afda35a59c9b562f202d95e190
My Username <Vxrpenter@users.noreply.github.com>
2025-04-02 22:59:43 +0200

    Answer

--- src/main/kotlin/dev/vxrp/bot/events/modals/NoticeOfDepartureModals.kt
+++ src/main/kotlin/dev/vxrp/bot/events/modals/NoticeOfDepartureModals.kt
@@ -1,8 +1,10 @@
 package dev.vxrp.bot.events.modals
 
+import dev.minn.jda.ktx.messages.reply_
 import dev.vxrp.bot.noticeofdeparture.NoticeOfDepartureManager
 import dev.vxrp.configuration.loaders.Config
 import dev.vxrp.configuration.loaders.Translation
+import dev.vxrp.util.color.ColorTool
 import net.dv8tion.jda.api.events.interaction.ModalInteractionEvent
 
 class NoticeOfDepartureModals(val event: ModalInteractionEvent, val config: Config, val translation: Translation) {
@@ -12,6 +14,7 @@ class NoticeOfDepartureModals(val event: ModalInteractionEvent, val config: Conf
             val reason = event.values[1].asString
 
             NoticeOfDepartureManager(event.jda, config, translation).sendDecisionMessage(event.user.id, date, reason)
+            event.reply_(ColorTool().useCustomColorCodes(translation.noticeOfDeparture.messageDescisionSent)).queue()
         }
     }
 }
