5dfa88e3bd75cacdafda570a287ecac402771e1f
vxrpenter <david.reulen@web.de>
2025-04-03 01:02:39 +0200

    New database function for notice of departures

--- src/main/kotlin/dev/vxrp/database/tables/NoticeOfDepartureTable.kt
+++ src/main/kotlin/dev/vxrp/database/tables/NoticeOfDepartureTable.kt
@@ -1,20 +1,34 @@
 package dev.vxrp.database.tables
 
 import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.transactions.transaction
 
 class NoticeOfDepartureTable {
     object NoticeOfDepartures : Table("notice_of_departures") {
         val id = text("id")
-        val handler_id = text("handler_id")
-        val channel_id = text("channel_id")
-        val message_id = text("message_id")
-        val begin_date = text("begin_date")
-        val end_date = text("end_date")
+        val handlerId = text("handler_id")
+        val channelId = text("channel_id")
+        val messageId = text("message_id")
+        val beginDate = text("begin_date")
+        val endDate = text("end_date")
 
         override val primaryKey: PrimaryKey
             get() = PrimaryKey(id)
+    }
+
+    fun addToDatabase(noticeId: String, handler: String, channel: String, message: String, exactBeginDate: String, exactEndDate: String) {
+        transaction {
+            NoticeOfDepartures.insert {
+                it[id] = noticeId
+                it[handlerId] = handler
+                it[channelId] = channel
+                it[messageId] = message
+                it[beginDate] = exactBeginDate
+                it[endDate] = exactEndDate
+            }
+        }
     }
 
     fun retrieveSerial(): Long {
