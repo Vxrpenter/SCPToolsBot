210c06847bef6d523d3e340594743632e69339d8
My Username <Vxrpenter@users.noreply.github.com>
2025-04-04 22:15:30 +0200

    New database stuff

--- src/main/kotlin/dev/vxrp/database/tables/UserTable.kt
+++ src/main/kotlin/dev/vxrp/database/tables/UserTable.kt
@@ -1,6 +1,8 @@
 package dev.vxrp.database.tables
 
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
 import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.deleteWhere
 import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.transactions.transaction
@@ -26,6 +28,42 @@ class UserTable {
                 it[steamId] = userSteamId
                 it[refreshToken] = discordRefreshToken
             }
+        }
+    }
+
+    fun getVerifyTime(userId: String): String {
+        var verifiedTime: String? = null
+
+        transaction {
+            Users.selectAll()
+                .where { Users.id eq userId }
+                .forEach {
+                    verifiedTime = it[Users.verifyTime]
+                }
+        }
+
+        return verifiedTime!!
+    }
+
+    fun getSteamId(userId: String): String {
+        var steamId: String? = null
+
+        transaction {
+            Users.selectAll()
+                .where { Users.id eq userId }
+                .forEach {
+                    steamId = it[Users.steamId]
+                }
+        }
+
+        return steamId!!
+    }
+
+    fun delete(userId: String) {
+        if (exists(userId)) return
+
+        transaction {
+            Users.deleteWhere { id eq userId }
         }
     }
 
