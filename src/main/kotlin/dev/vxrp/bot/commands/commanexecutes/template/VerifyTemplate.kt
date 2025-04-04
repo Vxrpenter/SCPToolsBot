75053b441329d4c6ab74e73ad08ddde53a7f885c
My Username <Vxrpenter@users.noreply.github.com>
2025-04-04 18:34:40 +0200

    Verify template addition

--- src/main/kotlin/dev/vxrp/bot/commands/commanexecutes/template/VerifyTemplate.kt
+++ src/main/kotlin/dev/vxrp/bot/commands/commanexecutes/template/VerifyTemplate.kt
@@ -1,11 +1,26 @@
 package dev.vxrp.bot.commands.commanexecutes.template
 
+import dev.minn.jda.ktx.messages.Embed
+import dev.minn.jda.ktx.messages.send
 import dev.vxrp.configuration.loaders.Config
 import dev.vxrp.configuration.loaders.Translation
+import dev.vxrp.util.color.ColorTool
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
+import net.dv8tion.jda.api.interactions.components.buttons.Button
 
-class VerifyTemplate(config: Config, translation: Translation) {
+class VerifyTemplate(val config: Config, val translation: Translation) {
     fun pasteTemplate(event: SlashCommandInteractionEvent) {
+        val embed = Embed {
+            title = ColorTool().useCustomColorCodes(translation.verify.embedTemplateTitle)
+            description = ColorTool().useCustomColorCodes(translation.verify.embedTemplateBody)
+        }
 
+        event.channel.send("", listOf(embed)).setActionRow(
+            Button.link(config.settings.verify.oauthLink, translation.buttons.textVerifyVerify),
+            Button.secondary("verify_show_data", translation.buttons.textVerifyShowData),
+            Button.danger("verify_delete", translation.buttons.textVerifyDelete)
+        ).queue()
+
+        event.reply("Created verify template").setEphemeral(true).queue()
     }
 }
