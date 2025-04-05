87465148790e9308faaaad4db3025c93b32b70a5
My Username <Vxrpenter@users.noreply.github.com>
2025-04-05 02:53:45 +0200

    RegularsMessageHandler addition

---
+++ src/main/kotlin/dev/vxrp/bot/regulars/RegularsMessageHandler.kt
@@ -0,0 +1,40 @@
+package dev.vxrp.bot.regulars
+
+import dev.minn.jda.ktx.messages.Embed
+import dev.minn.jda.ktx.messages.send
+import dev.vxrp.configuration.loaders.Config
+import dev.vxrp.configuration.loaders.Translation
+import dev.vxrp.util.color.ColorTool
+import net.dv8tion.jda.api.JDA
+import net.dv8tion.jda.api.entities.MessageEmbed
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel
+
+class RegularsMessageHandler(val api: JDA, val config: Config, val translation: Translation) {
+    fun sendRegulars(channel: TextChannel) {
+        val embeds = mutableListOf<MessageEmbed>()
+
+        val regulars = RegularsManager(config, translation).query()
+
+        for (regular in regulars) {
+            val stringBuilder: StringBuilder = StringBuilder()
+
+            for (role in regular.config.roles) {
+                stringBuilder.append(ColorTool().useCustomColorCodes(translation.regulars.embedTemplateRoleBody
+                    .replace("%role%", role.id)
+                    .replace("%description%", role.description)
+                    .replace("%timeframe%", role.playtimeRequirements.toString()+"h")))
+            }
+
+            val groupEmbed = Embed {
+                description = ColorTool().useCustomColorCodes(translation.regulars.embedTemplateGroupBody
+                    .replace("%description%", regular.manifest.description)
+                    .replace("%group_role%", "<@&${regular.manifest.customRole.id}>")
+                    .replace("%roles%", stringBuilder.toString()))
+            }
+
+            embeds.add(groupEmbed)
+        }
+
+        channel.send("", embeds).queue()
+    }
+}
