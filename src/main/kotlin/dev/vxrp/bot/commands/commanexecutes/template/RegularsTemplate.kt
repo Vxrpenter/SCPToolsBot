ad91fc367e07940074dc5a24dac6c067b7d5454d
My Username <Vxrpenter@users.noreply.github.com>
2025-04-05 14:03:47 +0200

    Regulars template addition

--- src/main/kotlin/dev/vxrp/bot/commands/commanexecutes/template/RegularsTemplate.kt
+++ src/main/kotlin/dev/vxrp/bot/commands/commanexecutes/template/RegularsTemplate.kt
@@ -1,12 +1,14 @@
 package dev.vxrp.bot.commands.commanexecutes.template
 
-import dev.minn.jda.ktx.messages.Embed
+import dev.vxrp.bot.regulars.RegularsMessageHandler
 import dev.vxrp.configuration.loaders.Config
 import dev.vxrp.configuration.loaders.Translation
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
 
 class RegularsTemplate(val config: Config, val translation: Translation) {
     fun pasteTemplate(event: SlashCommandInteractionEvent) {
+        RegularsMessageHandler(event.jda, config, translation).sendRegulars(event.channel.asTextChannel())
 
+        event.reply("Created regulars template").setEphemeral(true).queue()
     }
 }
