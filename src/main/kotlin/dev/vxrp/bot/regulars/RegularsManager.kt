24e039a39acecb33e7bb240c5b5f3ed8301c5ae8
My Username <Vxrpenter@users.noreply.github.com>
2025-04-05 16:35:21 +0200

    Some more additions

--- src/main/kotlin/dev/vxrp/bot/regulars/RegularsManager.kt
+++ src/main/kotlin/dev/vxrp/bot/regulars/RegularsManager.kt
@@ -1,6 +1,8 @@
 package dev.vxrp.bot.regulars
 import dev.vxrp.configuration.loaders.Config
 import dev.vxrp.configuration.loaders.Translation
+import dev.vxrp.database.tables.RegularsTable
+import java.time.LocalDate
 
 class RegularsManager(val config: Config, val translation: Translation) {
 
@@ -10,18 +12,35 @@ class RegularsManager(val config: Config, val translation: Translation) {
     }
 
     fun syncRegulars(userId: String, group: String) {
+        val configQuery = RegularsFileHandler(config, translation).query()
 
+        var groupRoleId: String? = null
+        var roleId: String? = null
+
+        for (folderGroup in configQuery) {
+            if (folderGroup.manifest.name != group) continue
+
+            if (folderGroup.manifest.customRole.use) groupRoleId = folderGroup.manifest.customRole.id
+
+            for (role in folderGroup.config.roles) {
+                roleId = role.id
+                break
+            }
+            break
+        }
+
+        RegularsTable().addToDatabase(userId, true, group, groupRoleId, roleId!!, 0.0, LocalDate.now().toString())
     }
 
     fun reactivateSync(userId: String) {
-
+        RegularsTable().setActive(userId, true)
     }
 
     fun deactivateSync(userId: String) {
-
+        RegularsTable().setActive(userId, false)
     }
 
     fun removeSync(userId: String) {
-
+        RegularsTable().delete(userId)
     }
 }
