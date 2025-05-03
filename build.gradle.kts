4029fea885ea11c927cdb8e01c4ba28b9b048f8a
My Username <Vxrpenter@users.noreply.github.com>
2025-05-03 13:57:26 +0200

    New jda version

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,7 +31,7 @@ val mariaDBVersion = "3.5.3"
 
 val kotlinxSerializationVersion = "1.8.1"
 val kamlVersion = "0.77.0"
-val jdaVersion = "5.4.0"
+val jdaVersion = "5.5.1"
 val jdaKtxVersion = "0.12.0"
 val logbackVersion = "1.5.18"
 val apacheCommonsVersion = "3.4"
