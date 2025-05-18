0fb395593cc531f678c425a299eaa8a4d769250b
vxrpenter <david.reulen@web.de>
2025-05-18 22:03:57 +0200

    Fix #13, no longer violating role 8.8 out of the VSC

--- src/main/kotlin/dev/vxrp/bot/commands/handler/status/playerlist/PlayerlistMessageHandler.kt
+++ src/main/kotlin/dev/vxrp/bot/commands/handler/status/playerlist/PlayerlistMessageHandler.kt
@@ -20,8 +20,7 @@ class PlayerlistMessageHandler {
 
                 builder.append(
                     ColorTool().useCustomColorCodes(translation.status.embedPlayerlistPlayer
-                            .replace("%nickname%", player.nickname.toString())
-                            .replace("%id%", player.id.toString())))
+                            .replace("%nickname%", player.nickname.toString())))
             }
         } else {
             builder.append(ColorTool().useCustomColorCodes(translation.status.embedPlayerlistCouldntFetch).trimIndent())
@@ -38,31 +37,12 @@ class PlayerlistMessageHandler {
             description = ColorTool().useCustomColorCodes(
                 translation.status.embedPlayerlistBody
                     .replace("%players%", builder.toString())
-                    .replace(
-                        "%version%",
-                        statusConstructor.mappedServers[currentPort]?.version ?: "&red&&bold&Not Fetched"
-                    )
-                    .replace(
-                        "%player_number%",
-                        statusConstructor.mappedServers[currentPort]?.players?.split("/".toRegex())?.get(0)
-                            ?: 0.toString()
-                    )
-                    .replace(
-                        "%ff%",
-                        statusConstructor.mappedServers[currentPort]?.ff?.toString() ?: "&red&&bold&Not Fetched"
-                    )
-                    .replace(
-                        "%wl%",
-                        statusConstructor.mappedServers[currentPort]?.wl?.toString() ?: "&red&&bold&Not Fetched"
-                    )
-                    .replace(
-                        "%modded%",
-                        statusConstructor.mappedServers[currentPort]?.modded?.toString() ?: "&red&&bold&Not Fetched"
-                    )
-                    .replace(
-                        "%mods%",
-                        statusConstructor.mappedServers[currentPort]?.mods?.toString() ?: "&red&&bold&Not Fetched"
-                    )
+                    .replace("%version%", statusConstructor.mappedServers[currentPort]?.version ?: "&red&&bold&Not Fetched")
+                    .replace("%player_number%", statusConstructor.mappedServers[currentPort]?.players?.split("/".toRegex())?.get(0) ?: 0.toString())
+                    .replace("%ff%", statusConstructor.mappedServers[currentPort]?.ff?.toString() ?: "&red&&bold&Not Fetched")
+                    .replace("%wl%", statusConstructor.mappedServers[currentPort]?.wl?.toString() ?: "&red&&bold&Not Fetched")
+                    .replace("%modded%", statusConstructor.mappedServers[currentPort]?.modded?.toString() ?: "&red&&bold&Not Fetched")
+                    .replace("%mods%", statusConstructor.mappedServers[currentPort]?.mods?.toString() ?: "&red&&bold&Not Fetched")
                     //Styling for true and false
                     .replace("true", "&green&&bold&true")
                     .replace("false", "&red&&bold&false")
