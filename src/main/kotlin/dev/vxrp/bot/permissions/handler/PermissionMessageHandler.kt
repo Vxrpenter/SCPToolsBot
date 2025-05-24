0d103c3e69d90462551f5acf7766711025f9b3a6
vxrpenter <david.reulen@web.de>
2025-05-24 13:45:24 +0200

    New check status method to simplify deactivated feature interactions

--- src/main/kotlin/dev/vxrp/bot/permissions/handler/PermissionMessageHandler.kt
+++ src/main/kotlin/dev/vxrp/bot/permissions/handler/PermissionMessageHandler.kt
@@ -3,13 +3,14 @@ package dev.vxrp.bot.permissions.handler
 import dev.minn.jda.ktx.messages.Embed
 import dev.vxrp.bot.permissions.enums.PermissionMessageType
 import dev.vxrp.bot.permissions.enums.PermissionType
+import dev.vxrp.bot.permissions.enums.StatusMessageType
 import dev.vxrp.configuration.data.Config
 import dev.vxrp.configuration.data.Translation
 import dev.vxrp.util.color.ColorTool
 import net.dv8tion.jda.api.entities.MessageEmbed
 
 class PermissionMessageHandler(val config: Config, val translation: Translation) {
-    fun getSpecificMessage(permissionType: PermissionType): MessageEmbed? {
+    fun getPermissionMessage(permissionType: PermissionType): MessageEmbed? {
         when (permissionType) {
             PermissionType.TICKET -> {
                 return ticketMessage(PermissionMessageType.INSUFFICIENT_PERMISSION)
@@ -37,6 +38,34 @@ class PermissionMessageHandler(val config: Config, val translation: Translation)
         }
 
         return null
+    }
+
+    fun getStatusMessage(messageType: StatusMessageType): MessageEmbed {
+        when (messageType) {
+            StatusMessageType.PANEL -> {
+                return Embed {
+                    color = 0xE74D3C
+                    title = ColorTool().useCustomColorCodes(translation.permissions.embedCouldNotSendPanelTitle)
+                    description = ColorTool().useCustomColorCodes(translation.permissions.embedCouldNotSendPanelBody)
+                }
+            }
+
+            StatusMessageType.MODAL -> {
+                return Embed {
+                    color = 0xE74D3C
+                    title = ColorTool().useCustomColorCodes(translation.permissions.embedCouldNotSendModalTitle)
+                    description = ColorTool().useCustomColorCodes(translation.permissions.embedCouldNotSendModalBody)
+                }
+            }
+
+            StatusMessageType.TEMPLATE -> {
+                return Embed {
+                    color = 0xE74D3C
+                    title = ColorTool().useCustomColorCodes(translation.permissions.embedCouldNotSendTemplateTitle)
+                    description = ColorTool().useCustomColorCodes(translation.permissions.embedCouldNotSendTemplateBody)
+                }
+            }
+        }
     }
 
     private fun ticketMessage(permissionMessageType: PermissionMessageType): MessageEmbed {
