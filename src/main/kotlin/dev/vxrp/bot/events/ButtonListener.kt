a85d4c35146367f46dec6efa95c4dcc88af17bfd
My Username <Vxrpenter@users.noreply.github.com>
2025-05-24 14:46:39 +0200

    Better status checks for all events and commands

--- src/main/kotlin/dev/vxrp/bot/events/ButtonListener.kt
+++ src/main/kotlin/dev/vxrp/bot/events/ButtonListener.kt
@@ -1,7 +1,10 @@
 package dev.vxrp.bot.events
 
 import dev.minn.jda.ktx.events.listener
+import dev.minn.jda.ktx.messages.reply_
 import dev.vxrp.bot.events.buttons.*
+import dev.vxrp.bot.permissions.PermissionManager
+import dev.vxrp.bot.permissions.enums.StatusMessageType
 import dev.vxrp.configuration.data.Config
 import dev.vxrp.configuration.data.Translation
 import dev.vxrp.util.launch.LaunchOptionManager
@@ -16,17 +19,39 @@ class ButtonListener(val api: JDA, val config: Config, val translation: Translat
         api.listener<ButtonInteractionEvent> { event ->
             val launchOptionManager = LaunchOptionManager(config, translation)
 
-            if (launchOptionManager.checkSectionOption(LaunchOptionType.BUTTON_LISTENER, LaunchOptionSectionType.HELP_BUTTONS).engage) HelpButtons(event, config, translation).init()
+            if (launchOptionManager.checkSectionOption(LaunchOptionType.BUTTON_LISTENER, LaunchOptionSectionType.HELP_BUTTONS).engage && event.button.id!!.startsWith("help")) {
+                HelpButtons(event, config, translation).init()
+            }
 
-            if (launchOptionManager.checkSectionOption(LaunchOptionType.BUTTON_LISTENER, LaunchOptionSectionType.TICKET_BUTTONS).engage) TicketButtons(event, config, translation).init()
+            if (launchOptionManager.checkSectionOption(LaunchOptionType.BUTTON_LISTENER, LaunchOptionSectionType.TICKET_BUTTONS).engage && event.button.id!!.startsWith("ticket")) {
+                PermissionManager(config, translation).checkStatus(StatusMessageType.PANEL, config.ticket.settings.ticketLogChannel != "")?.let { embed ->
+                    event.reply_("", listOf(embed)).setEphemeral(true).queue()
+                } ?: TicketButtons(event, config, translation).init()
+            }
 
-            if (launchOptionManager.checkSectionOption(LaunchOptionType.BUTTON_LISTENER, LaunchOptionSectionType.APPLICATION_BUTTONS).engage) ApplicationButtons(event, config, translation).init()
+            if (launchOptionManager.checkSectionOption(LaunchOptionType.BUTTON_LISTENER, LaunchOptionSectionType.APPLICATION_BUTTONS).engage && event.button.id!!.startsWith("application")) {
+                PermissionManager(config, translation).checkStatus(StatusMessageType.PANEL, config.ticket.settings.applicationMessageChannel != "")?.let { embed ->
+                    event.reply_("", listOf(embed)).setEphemeral(true).queue()
+                } ?: ApplicationButtons(event, config, translation).init()
+            }
 
-            if (launchOptionManager.checkSectionOption(LaunchOptionType.BUTTON_LISTENER, LaunchOptionSectionType.VERIFY_BUTTONS).engage) VerifyButtons(event, config, translation).init()
+            if (launchOptionManager.checkSectionOption(LaunchOptionType.BUTTON_LISTENER, LaunchOptionSectionType.VERIFY_BUTTONS).engage && event.button.id!!.startsWith("verify")) {
+                PermissionManager(config, translation).checkStatus(StatusMessageType.PANEL, config.settings.verify.active, config.settings.webserver.active)?.let { embed ->
+                    event.reply_("", listOf(embed)).setEphemeral(true).queue()
+                } ?: VerifyButtons(event, config, translation).init()
+            }
 
-            if (launchOptionManager.checkSectionOption(LaunchOptionType.BUTTON_LISTENER, LaunchOptionSectionType.NOTICE_OF_DEPARTURE_BUTTONS).engage) NoticeOfDepartureButtons(event, config, translation).init()
+            if (launchOptionManager.checkSectionOption(LaunchOptionType.BUTTON_LISTENER, LaunchOptionSectionType.NOTICE_OF_DEPARTURE_BUTTONS).engage && event.button.id!!.startsWith("notice_of_departure")) {
+                PermissionManager(config, translation).checkStatus(StatusMessageType.PANEL, config.settings.noticeOfDeparture.active)?.let { embed ->
+                    event.reply_("", listOf(embed)).setEphemeral(true).queue()
+                } ?: NoticeOfDepartureButtons(event, config, translation).init()
+            }
 
-            if (launchOptionManager.checkSectionOption(LaunchOptionType.BUTTON_LISTENER, LaunchOptionSectionType.REGULARS_BUTTONS).engage) RegularsButtons(event, config, translation).init()
+            if (launchOptionManager.checkSectionOption(LaunchOptionType.BUTTON_LISTENER, LaunchOptionSectionType.REGULARS_BUTTONS).engage && event.button.id!!.startsWith("regulars")) {
+                PermissionManager(config, translation).checkStatus(StatusMessageType.PANEL, config.settings.regulars.active, config.settings.verify.active, config.settings.webserver.active)?.let { embed ->
+                    event.reply_("", listOf(embed)).setEphemeral(true).queue()
+                } ?: RegularsButtons(event, config, translation).init()
+            }
         }
     }
 }
