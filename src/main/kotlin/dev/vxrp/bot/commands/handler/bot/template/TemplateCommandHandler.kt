26e25484bbc962b49a820956228ad589f46288f7
My Username <Vxrpenter@users.noreply.github.com>
2025-05-24 13:49:01 +0200

    Update to template handling

--- src/main/kotlin/dev/vxrp/bot/commands/handler/bot/template/TemplateCommandHandler.kt
+++ src/main/kotlin/dev/vxrp/bot/commands/handler/bot/template/TemplateCommandHandler.kt
@@ -4,29 +4,34 @@ import dev.vxrp.bot.commands.handler.bot.template.templates.NoticeOfDepartureTem
 import dev.vxrp.bot.commands.handler.bot.template.templates.RegularsTemplate
 import dev.vxrp.bot.commands.handler.bot.template.templates.SupportTemplate
 import dev.vxrp.bot.commands.handler.bot.template.templates.VerifyTemplate
+import dev.vxrp.bot.permissions.PermissionManager
+import dev.vxrp.bot.permissions.enums.StatusMessageType
 import dev.vxrp.configuration.data.Config
 import dev.vxrp.configuration.data.Translation
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
 
-class TemplateCommandHandler(val config: Config, private val translations:Translation) {
+class TemplateCommandHandler(val config: Config, private val translation: Translation) {
     fun findOption(event: SlashCommandInteractionEvent) {
         val option = event.getOption("template")?.asString
 
         when (option) {
             "support" -> {
-                SupportTemplate(config, translations).pasteTemplate(event)
+                SupportTemplate(config, translation).pasteTemplate(event)
             }
 
             "verify" -> {
-                VerifyTemplate(config, translations).pasteTemplate(event)
+                if (!PermissionManager(config, translation).checkStatus(event.hook, StatusMessageType.TEMPLATE, config.settings.verify.active)) return
+                VerifyTemplate(config, translation).pasteTemplate(event)
             }
 
             "notice_of_departure" -> {
-                NoticeOfDepartureTemplate(config, translations).pasteTemplate(event)
+                if (!PermissionManager(config, translation).checkStatus(event.hook, StatusMessageType.TEMPLATE, config.settings.noticeOfDeparture.active)) return
+                NoticeOfDepartureTemplate(config, translation).pasteTemplate(event)
             }
 
             "regulars" -> {
-                RegularsTemplate(config, translations).pasteTemplate(event)
+                if (!PermissionManager(config, translation).checkStatus(event.hook, StatusMessageType.TEMPLATE, config.settings.regulars.active)) return
+                RegularsTemplate(config, translation).pasteTemplate(event)
             }
         }
     }
