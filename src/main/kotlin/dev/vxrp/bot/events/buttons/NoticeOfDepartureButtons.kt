223118d355a9217a31f6e4c1538b9023c9b22f74
My Username <Vxrpenter@users.noreply.github.com>
2025-05-24 13:26:47 +0200

    Checkstatus for notice of departure

--- src/main/kotlin/dev/vxrp/bot/events/buttons/NoticeOfDepartureButtons.kt
+++ src/main/kotlin/dev/vxrp/bot/events/buttons/NoticeOfDepartureButtons.kt
@@ -1,5 +1,6 @@
 package dev.vxrp.bot.events.buttons
 
+import dev.minn.jda.ktx.messages.Embed
 import dev.minn.jda.ktx.messages.reply_
 import dev.vxrp.bot.modals.NoticeOfDepartureTemplateModals
 import dev.vxrp.bot.noticeofdeparture.enums.ActionId
@@ -7,15 +8,18 @@ import dev.vxrp.bot.permissions.PermissionManager
 import dev.vxrp.bot.permissions.enums.PermissionType
 import dev.vxrp.configuration.data.Config
 import dev.vxrp.configuration.data.Translation
+import dev.vxrp.util.color.ColorTool
 import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent
 
 class NoticeOfDepartureButtons(val event: ButtonInteractionEvent, val config: Config, val translation: Translation) {
     suspend fun init() {
         if (event.button.id?.startsWith("file_notice_of_departure") == true) {
+            if (!checkStatus()) return
             event.replyModal(NoticeOfDepartureTemplateModals(translation).generalModal()).queue()
         }
 
         if (event.button.id?.startsWith("notice_of_departure_decision_accept") == true) {
+            if (!checkStatus()) return
             if(permissionCheck(PermissionType.NOTICE_OF_DEPARTURES)) return
             val splittetId = event.button.id!!.split(":")
 
@@ -26,6 +30,7 @@ class NoticeOfDepartureButtons(val event: ButtonInteractionEvent, val config: Co
         }
 
         if (event.button.id?.startsWith("notice_of_departure_decision_dismiss") == true) {
+            if (!checkStatus()) return
             if(permissionCheck(PermissionType.NOTICE_OF_DEPARTURES)) return
             val splittetId = event.button.id!!.split(":")
 
@@ -36,6 +41,7 @@ class NoticeOfDepartureButtons(val event: ButtonInteractionEvent, val config: Co
         }
 
         if (event.button.id?.startsWith("notice_of_departure_revoke") == true) {
+            if (!checkStatus()) return
             if(permissionCheck(PermissionType.NOTICE_OF_DEPARTURES)) return
             val splittetId = event.button.id!!.split(":")
 
@@ -53,5 +59,19 @@ class NoticeOfDepartureButtons(val event: ButtonInteractionEvent, val config: Co
         }
         if (!permissionPair.first) return true
         return false
+    }
+
+    private fun checkStatus(): Boolean {
+        if (!config.settings.noticeOfDeparture.active) {
+            val embed = Embed {
+                color = 0xE74D3C
+                title = ColorTool().useCustomColorCodes(translation.permissions.embedCouldNotSendModalTitle)
+                description = ColorTool().useCustomColorCodes(translation.permissions.embedCouldNotSendModalBody)
+            }
+
+            event.reply_("", listOf(embed)).setEphemeral(true).queue()
+            return false
+        }
+        return true
     }
 }
