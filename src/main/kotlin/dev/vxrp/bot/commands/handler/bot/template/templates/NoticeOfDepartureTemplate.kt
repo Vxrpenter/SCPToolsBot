5cf2a66471fa39ee65a6b5e3e492d0be79d00ab1
My Username <Vxrpenter@users.noreply.github.com>
2025-05-25 17:17:07 +0200

    Optimizing imports

--- src/main/kotlin/dev/vxrp/bot/commands/handler/bot/template/templates/NoticeOfDepartureTemplate.kt
+++ src/main/kotlin/dev/vxrp/bot/commands/handler/bot/template/templates/NoticeOfDepartureTemplate.kt
@@ -1,15 +1,11 @@
 package dev.vxrp.bot.commands.handler.bot.template.templates
 
-import dev.minn.jda.ktx.messages.Embed
 import dev.minn.jda.ktx.messages.reply_
-import dev.minn.jda.ktx.messages.send
 import dev.vxrp.bot.noticeofdeparture.handler.NoticeOfDepartureMessageHandler
 import dev.vxrp.configuration.data.Config
 import dev.vxrp.configuration.data.Translation
 import dev.vxrp.util.color.ColorTool
-import net.dv8tion.jda.api.entities.emoji.Emoji
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
-import net.dv8tion.jda.api.interactions.components.buttons.Button
 
 class NoticeOfDepartureTemplate(val config: Config, val translation: Translation) {
     fun pasteTemplate(event: SlashCommandInteractionEvent) {
--- src/main/kotlin/dev/vxrp/bot/commands/handler/bot/template/templates/SupportTemplate.kt
+++ src/main/kotlin/dev/vxrp/bot/commands/handler/bot/template/templates/SupportTemplate.kt
@@ -1,15 +1,11 @@
 package dev.vxrp.bot.commands.handler.bot.template.templates
 
-import dev.minn.jda.ktx.messages.Embed
 import dev.minn.jda.ktx.messages.reply_
-import dev.minn.jda.ktx.messages.send
 import dev.vxrp.bot.ticket.handler.TicketMessageHandler
 import dev.vxrp.configuration.data.Config
 import dev.vxrp.configuration.data.Translation
 import dev.vxrp.util.color.ColorTool
-import net.dv8tion.jda.api.entities.emoji.Emoji
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
-import net.dv8tion.jda.api.interactions.components.selections.StringSelectMenu
 
 class SupportTemplate(val config: Config, val translation: Translation) {
     fun pasteTemplate(event: SlashCommandInteractionEvent) {
