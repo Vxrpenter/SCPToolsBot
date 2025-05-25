c5d354eb7409a32e1be9f2dad15611a2c224e056
My Username <Vxrpenter@users.noreply.github.com>
2025-05-25 16:57:33 +0200

    Moved all templates to their respective message handlers

--- src/main/kotlin/dev/vxrp/bot/commands/handler/bot/template/templates/VerifyTemplate.kt
+++ src/main/kotlin/dev/vxrp/bot/commands/handler/bot/template/templates/VerifyTemplate.kt
@@ -2,6 +2,7 @@ package dev.vxrp.bot.commands.handler.bot.template.templates
 
 import dev.minn.jda.ktx.messages.Embed
 import dev.minn.jda.ktx.messages.send
+import dev.vxrp.bot.verify.VerifyMessageHandler
 import dev.vxrp.configuration.data.Config
 import dev.vxrp.configuration.data.Translation
 import dev.vxrp.util.color.ColorTool
@@ -10,17 +11,7 @@ import net.dv8tion.jda.api.interactions.components.buttons.Button
 
 class VerifyTemplate(val config: Config, val translation: Translation) {
     fun pasteTemplate(event: SlashCommandInteractionEvent) {
-        val embed = Embed {
-            thumbnail = event.guild?.iconUrl
-            title = ColorTool().useCustomColorCodes(translation.verify.embedTemplateTitle)
-            description = ColorTool().useCustomColorCodes(translation.verify.embedTemplateBody)
-        }
-
-        event.channel.send("", listOf(embed)).setActionRow(
-            Button.link(config.settings.verify.oauthLink, translation.buttons.textVerifyVerify),
-            Button.secondary("verify_show_data", translation.buttons.textVerifyShowData),
-            Button.danger("verify_delete", translation.buttons.textVerifyDelete)
-        ).queue()
+        VerifyMessageHandler(event.jda, config, translation).sendTemplate(event.channel.asTextChannel(), event.guild!!)
 
         event.reply("Created verify template").setEphemeral(true).queue()
     }
