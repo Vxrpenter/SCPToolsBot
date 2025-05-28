363970b790ce9f881e5d4319fc839f171bc6e404
My Username <Vxrpenter@users.noreply.github.com>
2025-05-28 16:43:35 +0200

    Pre-implementation of notice of departure command methods

---
+++ src/main/kotlin/dev/vxrp/bot/commands/handler/bot/noticeofdeparture/NoticeOfDepartureCommand.kt
@@ -0,0 +1,15 @@
+package dev.vxrp.bot.commands.handler.bot.noticeofdeparture
+
+import dev.vxrp.configuration.data.Config
+import dev.vxrp.configuration.data.Translation
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
+
+class NoticeOfDepartureCommand(val config: Config,  translation: Translation) {
+    fun view(event: SlashCommandInteractionEvent) {
+
+    }
+
+    fun revoke(event: SlashCommandInteractionEvent) {
+
+    }
+}
