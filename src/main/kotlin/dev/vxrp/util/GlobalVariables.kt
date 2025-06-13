91a28a4ed79563e96c27bbe9c298e9a922f01afc
My Username <Vxrpenter@users.noreply.github.com>
2025-06-14 00:33:22 +0200

    New global variables

--- src/main/kotlin/dev/vxrp/util/GlobalVariables.kt
+++ src/main/kotlin/dev/vxrp/util/GlobalVariables.kt
@@ -13,10 +13,22 @@ var statusMappedBots = hashMapOf<String, Int>()
  * Saves server information in correlation to its respective SCP: Secret Laboratory
  * server port
  */
-var statusMappedServers = hashMapOf<Int, Server>()
+var statusMappedServers = hashMapOf<Int, Server?>()
 
 /**
  * Saves the instance of a status bot in correlation to its respective SCP: Secret Laboratory
  * server port
  */
 var statusInstances = hashMapOf<Int, Instance>()
+
+/**
+ * Saves the current api sessions status (true: online, false: offline) to allow session and database
+ * status comparing
+ */
+var statusApiSessionStatus: Boolean = true
+
+/**
+ * Saves the current server sessions status (true: online, false: offline) in correlation to its respective
+ * SCP: Secret Laboratory, to allow session and database status comparing
+ */
+var statusServerSessionStatus = hashMapOf<Int, Boolean>()
