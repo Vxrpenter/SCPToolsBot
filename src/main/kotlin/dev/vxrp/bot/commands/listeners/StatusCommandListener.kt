e3ab95c6e0a500993b8c25eaf6e6f2af4137bd90
vxrpenter <david.reulen@web.de>
2025-06-19 12:54:46 +0200

    Auto deny commands send in private channels

--- src/main/kotlin/dev/vxrp/bot/commands/listeners/StatusCommandListener.kt
+++ src/main/kotlin/dev/vxrp/bot/commands/listeners/StatusCommandListener.kt
@@ -10,6 +10,7 @@ import dev.vxrp.util.launch.LaunchOptionManager
 import dev.vxrp.util.launch.enums.LaunchOptionSectionType
 import dev.vxrp.util.launch.enums.LaunchOptionType
 import net.dv8tion.jda.api.JDA
+import net.dv8tion.jda.api.entities.channel.ChannelType
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
 
@@ -17,6 +18,7 @@ class StatusCommandListener(val api: JDA, val config: Config, val translation: T
 
     init {
         api.listener<SlashCommandInteractionEvent> { event ->
+            if (event.channel.type == ChannelType.PRIVATE) return@listener
             val commandList = config.extra.commands.statusCommands
 
             for (command in commandList) {
