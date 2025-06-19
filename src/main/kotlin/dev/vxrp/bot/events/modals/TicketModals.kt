995dc0debda7f88b6083feb3fca8c177572bf934
vxrpenter <david.reulen@web.de>
2025-06-19 12:50:50 +0200

    Remove unused imports

--- src/main/kotlin/dev/vxrp/bot/events/modals/TicketModals.kt
+++ src/main/kotlin/dev/vxrp/bot/events/modals/TicketModals.kt
@@ -1,9 +1,7 @@
 package dev.vxrp.bot.events.modals
 
 import dev.minn.jda.ktx.messages.Embed
-import dev.minn.jda.ktx.messages.reply_
 import dev.minn.jda.ktx.messages.send
-import dev.vxrp.bot.permissions.enums.PermissionType
 import dev.vxrp.bot.ticket.TicketManager
 import dev.vxrp.bot.ticket.enums.TicketStatus
 import dev.vxrp.bot.ticket.enums.TicketType
