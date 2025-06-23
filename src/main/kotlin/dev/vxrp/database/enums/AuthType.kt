814f790dd1b4524123e0267e7bb811cd92088337
My Username <Vxrpenter@users.noreply.github.com>
2025-06-23 22:50:49 +0200

    Copyright notice addition

--- src/main/kotlin/dev/vxrp/database/enums/AuthType.kt
+++ src/main/kotlin/dev/vxrp/database/enums/AuthType.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2024 Vxrpenter and the SCPToolsBot Contributors
+ *
+ * Licenced under the MIT License, any non-license compliant usage of this file(s) content
+ * is prohibited. If you did not receive a copy of the license with this file, you
+ *  may obtain the license at
+ *
+ *  https://mit-license.org/
+ *
+ *  This software may be used commercially if the usage is license compliant. The software
+ *  is provided without any sort of WARRANTY, and the authors cannot be held liable for
+ *  any form of claim, damages or other liabilities.
+ *
+ *  Note: This is no legal advice, please read the license conditions
+ */
+
 package dev.vxrp.database.enums
 
 enum class AuthType {
