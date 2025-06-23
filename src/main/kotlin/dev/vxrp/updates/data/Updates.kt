abd28f794a618e9c941cb093152c5d81b875a155
vxrpenter <david.reulen@web.de>
2025-06-23 17:00:00 +0200

    Small fixes to updates.json Serialization and config

--- src/main/kotlin/dev/vxrp/updates/data/Updates.kt
+++ src/main/kotlin/dev/vxrp/updates/data/Updates.kt
@@ -5,22 +5,10 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class Updates(
     val version: String,
-    val settings: UpdatesSettings,
     val configurationUpdate: List<UpdatesConfigurationSegment>,
     val translationUpdates: List<UpdatesConfigurationSegment>,
     val regularsUpdate: List<UpdatesConfigurationSegment>,
     val additionalInformation: String
-)
-
-@Serializable
-data class UpdatesSettings(
-    val regenerateConfig: Boolean,
-    val regenerateStatusSettings: Boolean,
-    val regenerateTicketSettings: Boolean,
-    val regenerateCommands: Boolean,
-    val regenerateLaunchConfiguration: Boolean,
-
-    val regenerateTranslations: Boolean
 )
 
 @Serializable
