c82fd0bce52ed82138bb7022671e5c0f302c9f2f
vxrpenter <david.reulen@web.de>
2025-06-23 17:00:38 +0200

    UpdatesFileHandler fixes for the new config file

--- src/main/kotlin/dev/vxrp/configuration/storage/ConfigPaths.kt
+++ src/main/kotlin/dev/vxrp/configuration/storage/ConfigPaths.kt
@@ -2,13 +2,18 @@ package dev.vxrp.configuration.storage
 
 import kotlin.io.path.Path
 
+/**
+ * These are all major configuration paths for important files.
+ * They are updated on every start using UpdatesFileHandler().setConfigPaths().
+ * If this update does not happen, they will fall back to these backup paths.
+ */
 class ConfigPaths {
-    val configPath = Path("/SCPToolsBot/configs/config.yml")
-    val ticketPath = Path("/SCPToolsBot/configs/ticket-settings.yml")
-    val statusPath = Path("/SCPToolsBot/configs/status-settings.yml")
-    val commandsPath = Path("/SCPToolsBot/configs/extra/commands.json")
-    val launchConfigurationPath = Path("/SCPToolsBot/configs/extra/launch-configuration.json")
+    var configPath = Path("/SCPToolsBot/configs/config.yml")
+    var ticketPath = Path("/SCPToolsBot/configs/ticket-settings.yml")
+    var statusPath = Path("/SCPToolsBot/configs/status-settings.yml")
+    var commandsPath = Path("/SCPToolsBot/configs/extra/commands.json")
+    var launchConfigurationPath = Path("/SCPToolsBot/configs/extra/launch-configuration.json")
 
-    val enUsPath = Path("/SCPToolsBot/lang/en_US.yml")
-    val deDePath = Path("/SCPToolsBot/lang/de_DE.yml")
+    var enUsPath = Path("/SCPToolsBot/lang/en_US.yml")
+    var deDePath = Path("/SCPToolsBot/lang/de_DE.yml")
 }
