7f7fa4e0368aa5002a0cac6bdae274d8f0f79fa6
vxrpenter <david.reulen@web.de>
2025-06-24 15:35:24 +0200

    Small copyright fix

--- src/main/kotlin/dev/vxrp/database/DatabaseManager.kt
+++ src/main/kotlin/dev/vxrp/database/DatabaseManager.kt
@@ -3,15 +3,15 @@
  *
  * Licenced under the MIT License, any non-license compliant usage of this file(s) content
  * is prohibited. If you did not receive a copy of the license with this file, you
- *  may obtain the license at
+ * may obtain the license at
  *
  *  https://mit-license.org/
  *
- *  This software may be used commercially if the usage is license compliant. The software
- *  is provided without any sort of WARRANTY, and the authors cannot be held liable for
- *  any form of claim, damages or other liabilities.
+ * This software may be used commercially if the usage is license compliant. The software
+ * is provided without any sort of WARRANTY, and the authors cannot be held liable for
+ * any form of claim, damages or other liabilities.
  *
- *  Note: This is no legal advice, please read the license conditions
+ * Note: This is no legal advice, please read the license conditions
  */
 
 package dev.vxrp.database
