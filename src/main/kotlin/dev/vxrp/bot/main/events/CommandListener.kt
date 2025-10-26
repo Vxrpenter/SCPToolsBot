059078a960af324fd4be2dcf4dd3e99ef9c16b0e
vxrpenter <david.reulen@web.de>
2025-10-26 17:13:33 +0100

    Adding base listeners

---
+++ src/main/kotlin/dev/vxrp/bot/main/events/CommandListener.kt
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2024 Vxrpenter and the SCPToolsBot Contributors
+ *
+ * Licenced under the MIT License, any non-license compliant usage of this file(s) content
+ * is prohibited. If you did not receive a copy of the license with this file, you
+ * may obtain the license at
+ *
+ *  https://mit-license.org/
+ *
+ * This software may be used commercially if the usage is license compliant. The software
+ * is provided without any sort of WARRANTY, and the authors cannot be held liable for
+ * any form of claim, damages or other liabilities.
+ *
+ * Note: This is no legal advice, please read the license conditions
+ */
+
+package dev.vxrp.bot.main.events
+
+import dev.minn.jda.ktx.events.listener
+import dev.vxrp.configuration.Config
+import dev.vxrp.configuration.Translation
+import net.dv8tion.jda.api.JDA
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
+import net.dv8tion.jda.api.hooks.ListenerAdapter
+
+class CommandListener(val api: JDA, val config: Config, val translation: Translation) : ListenerAdapter() {
+    init { api.listener<SlashCommandInteractionEvent> { event ->
+
+    }}
+}
